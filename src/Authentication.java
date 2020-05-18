8458fda9f629ad9fa5c10bf74335cc1210ab1169
EMRCHI001 <53513712+EMRCHI001@users.noreply.github.com>
2020-05-18 17:32:53 +0200

    Full implementation of authenticate and sign methods

--- src/Authentication.java
+++ src/Authentication.java
@@ -8,27 +8,48 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Authentication {
+    private static int SIGNATURE_SIZE = 256;
 
+    /**
+     * signs the input plaintext, using the provided private key
+     * @params  privateKey  the private key used for the signature
+     * @params  plaintext   the message to be signed
+     * @return  authentication signature using the private key
+     */
     public static String sign(String privateKey, String plaintext){
         String msghash = hash(plaintext);
-        //sig = encrypt(privateKey, msghash)
-        //return sig
-        return null; //[--temp]
+        String sig = Encryption.encrypt(privateKey, msghash);
+        /*debug --*/ System.out.printf("plaintext: %s -> signature %s%n", plaintext,sig);
+        return sig;
     }
 
+    /**
+     * authenticates the validity of the input message
+     * @params  publicKey public key of the sender
+     * @params  message   message to be authenticated
+     * @return  returns true if message is authentic, false otherwise
+     */
     public static boolean authenticate(String publicKey, String message) {
-        String dcmsg = Encryption.decompress(message); //dcmsg = plaintext | sig
+        byte [] msg = message.getBytes();
+        /*debug --*/ System.out.printf("Compressed message: %s%n",message);
+        String dcmsg = Encryption.decompress(msg); //dcmsg = plaintext | sig
         /*debug --*/ System.out.printf("Decompressed message -> %s%n",dcmsg);
-        //sig = extractSignature(dcmsg)
-        //plaintext = extractPlaintext(dcmsg)
+        String sig = extractSignature(dcmsg);
+        String plaintext = extractPlaintext(dcmsg);
         String oghash = Encryption.decrypt(sig, publicKey);
-        //String myhash = hash(plaintext);
-        //return oghash.compare(myhash);
-        // /*debug --*/ System.out.printf("(original hash) %s == (calculated hash) %s%n",oghash,myhash);
-        // /*debug --*/ System.out.printf("Authentication result: %b%n", oghash.compare(myhash));
-        return true; //[--temp]
+        String myhash = hash(plaintext);
+        /*debug --*/ System.out.printf("(original hash) %s == (calculated hash) %s%n",oghash,myhash);
+        /*debug --*/ System.out.printf("Authentication result: %b%n", oghash.equals(myhash));
+        return oghash.equals(myhash);
     }
 
+    /**
+     * creates a 256 bit message digest ("hash") of plaintext using SHA-256
+     * algorithm.
+     * @params  plaintext   plaintext to be hashed
+     * @return  returns a 256 bit hash of the plain text provided
+     * @exception NoSuchAlgorithmException on hashing algorithm
+     */
     public static String hash(String plaintext){
         // Using SHA-256 algorithm to generate a 256 bit hash
         try {
@@ -36,6 +57,7 @@ public class Authentication {
             byte[] bHash = md.digest(plaintext.getBytes());
             String sHash = new String(bHash);
             /*debug --*/ System.out.printf("(plaintext) %s -> (hash) %s%n",plaintext,sHash);
+            /*debug --*/ System.out.printf("Number of bits in hash: %d%n",bHash.length*8);
             return sHash;
         }
         catch (NoSuchAlgorithmException e) {
@@ -44,4 +66,13 @@ public class Authentication {
         return null;
     }
 
+    public static String extractPlaintext(String message){
+        // everything minus the last 256 bits
+        return ""; //[--temp]
+    }
+
+    public static String extractSignature(String message){
+        // the last 256 bits
+        return ""; //[--temp]
+    }
 }
