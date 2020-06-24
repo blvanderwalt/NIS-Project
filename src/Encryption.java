7e575a9eccc3daf4eaf7655e53ab932a8730518d
Ryan McCarlie <minimac321@gmail.com>
2020-06-24 12:36:42 +0200

    Add Javadoc comments to Encryption Class

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,31 +1,27 @@
 
-// NIS 2020
-// Encryption Class
-// -- Performs the compression and encryption services
-//Authors:  Chiadika Emeruem, Ryan McCarlie, Ceara Mullins, Brent van der Walt
+/**
+ * Encryption Class
+ * Performs the compression and encryption services
+ * Authors:  Chiadika Emeruem, Ryan McCarlie, Ceara Mullins, Brent van der Walt
+ */
 
-//for encryption/decryption
 import org.bouncycastle.jcajce.provider.symmetric.AES;
-
 import java.security.*;
 import java.util.ArrayList;
-
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-
-//for (de)compression
-import java.util.Arrays;
 import java.util.zip.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class Encryption {
-    // --- Encryptionion and Decryption --- //
 
     /**
-     * Take in the message hash and private key
-     * @return signature in a byte[]
+     * Encrypts the message hash with the private key using the Cipher library
+     * @param msghash to extract signature from
+     * @param privateKey private key for encryption
+     * @return signature in a byte array
     */
     public static byte[] extractSig(byte[] msghash, PrivateKey privateKey) {
         byte[] signed_hash = null;
@@ -36,13 +32,14 @@ public class Encryption {
         }catch(NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e){
             e.printStackTrace();
         }
-
         return signed_hash;
     }
 
     /**
-     * Take in the message hash and public key
-     * @return signature in a byte[]
+     * Encrypts the message hash with the public key using the Cipher library
+     * @param msghash to extract signature from
+     * @param publicKey public key for encryption
+     * @return signature in a byte array
      */
     public static byte[] extractSig(byte[] msghash, PublicKey publicKey) {
         byte[] signed_hash = null;
@@ -56,7 +53,15 @@ public class Encryption {
 
         return signed_hash;
     }
-
+    /**
+     * Encrypts the secret key with the RSA algorithm
+     * Encrypts the message with the AES algorithm
+     * @param secretKey secret key used for RSA encryption
+     * @param init_vect initialization vector used in AES encryption
+     * @param publicKey public key used for AES encryption
+     * @param message plaintext message to be encrypted
+     * @return the encrypted secret key, initialization vector and message in a packaged byte array
+     */
     public static byte[] encrypt(SecretKey secretKey, byte[] init_vect, PublicKey publicKey, byte[] message)
             throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException{
 
@@ -66,12 +71,9 @@ public class Encryption {
 
         Cipher cipherRSA = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         cipherRSA.init(Cipher.ENCRYPT_MODE, publicKey);
-//        System.out.printf("Size of unencrypted shared key: %d\n", secretKey.getEncoded().length);
         byte[] bytes_sharedKey = cipherRSA.doFinal(secretKey.getEncoded());
-//        System.out.printf("Size of encrypted shared key: %d\n", bytes_sharedKey.length);
 
         byteOut.write(bytes_sharedKey);
-//        System.out.printf("Size of init_vector: %d\n", init_vect.length);
         byteOut.write(init_vect);
 
         // Encrypt File content using AES key
@@ -93,16 +95,19 @@ public class Encryption {
         if (AES_encryptedPayload!=null) {
             byteOut.write(AES_encryptedPayload);
         }
-
         byte[] encryptedPackage = new byte[byteOut.size()];
         encryptedPackage = byteOut.toByteArray();
-
-//        System.out.printf("Size of unencrypted msg: %d\n", message.length);
-//        System.out.printf("Size of encrypted msg: %d\n", encryptedPackage.length);
 
         return encryptedPackage;
     }
 
+    /**
+     * Decrypts the encrypted package using RSA and retires the secret key
+     * Decrypts the ciphertext message using AES to retrieve the plaintext
+     * @param privateKey private key used for AES encryption
+     * @param encryptedPackage byte package of the encrypted shared key, initialization vector and AES encrypted payload
+     * @return the plaintext message in a byte array
+     */
     public static byte[] decrypt(PrivateKey privateKey, byte[] encryptedPackage) throws IOException, NoSuchPaddingException,
             NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException,
             InvalidAlgorithmParameterException {
@@ -139,27 +144,18 @@ public class Encryption {
         if (AES_encryptedPayload!=null) {
             bOut.write(AES_encryptedPayload);
         }
-
         byte[] plainText = new byte[bOut.size()];
         plainText = bOut.toByteArray();
 
         return plainText;
     }
-
-    /**
-     * Process: RSA encryption has a very low limit for data that can be encrypted. therefore to encrypt larger sets of
-     * data we use symmetric encryption, AES, for encryption - THEN we encrypt the RSA for encrypting the AES key itself
-     *
-     * Takes a whole bunch of variables used to encryption AND the secret message
-     * created a file for the encryptedAES key and plaintext.txt for the secret message
-     *
-     * @return the encrypted sharedKey byte array.
-     */
-
     // --- Compression and Decompression --- //
 
-    // --- Compression --- //
-    // Compresses String message into a byte array with ZIP
+    /**
+     * Compresses String message with ZIP
+     * @param message String message to compressed
+     * @return compress message as a byte array
+     */
     public static byte [] compress (String message) {
         try {
             ByteArrayOutputStream byteArrOut = new ByteArrayOutputStream(message.length());
@@ -175,8 +171,11 @@ public class Encryption {
         }
     }
 
-    // --- Decompression --- //
-    // Decompresses the byte array into a string message
+    /**
+     * Decompresses the byte array using Zip
+     * @param compressage byte array to be decompressed
+     * @return decompressed String message
+     */
     public static String decompress (byte [] compressage) {
         try {
             ByteArrayInputStream byteArrIn = new ByteArrayInputStream(compressage);
